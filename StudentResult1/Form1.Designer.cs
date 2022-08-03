63dc34001f663bec3b622154ff3069526eceb409
IshanMark <rupasing-cs18036@stu.kln.ac.lk>
2022-08-03 09:50:52 +0530

    Add project files.

---
+++ StudentResult1/Form1.Designer.cs
@@ -0,0 +1,445 @@
+﻿namespace StudentResult1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label21 = new System.Windows.Forms.Label();
+            this.labTotal = new System.Windows.Forms.Label();
+            this.labAverage = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.btnComment = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.btnTotal = new System.Windows.Forms.Button();
+            this.btnGrade = new System.Windows.Forms.Button();
+            this.labReligion = new System.Windows.Forms.Label();
+            this.labHistory = new System.Windows.Forms.Label();
+            this.labScience = new System.Windows.Forms.Label();
+            this.labMatha = new System.Windows.Forms.Label();
+            this.labEnglish = new System.Windows.Forms.Label();
+            this.labSinhala = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtReligion = new System.Windows.Forms.TextBox();
+            this.txtHistory = new System.Windows.Forms.TextBox();
+            this.txtScience = new System.Windows.Forms.TextBox();
+            this.txtMatha = new System.Windows.Forms.TextBox();
+            this.txtEnglish = new System.Windows.Forms.TextBox();
+            this.txtSinhala = new System.Windows.Forms.TextBox();
+            this.txtStuNo = new System.Windows.Forms.TextBox();
+            this.txtStuName = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(272, 16);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(108, 13);
+            this.label21.TabIndex = 67;
+            this.label21.Text = "Student Result Sheet";
+            // 
+            // labTotal
+            // 
+            this.labTotal.AutoSize = true;
+            this.labTotal.Location = new System.Drawing.Point(130, 409);
+            this.labTotal.Name = "labTotal";
+            this.labTotal.Size = new System.Drawing.Size(25, 13);
+            this.labTotal.TabIndex = 66;
+            this.labTotal.Text = "600";
+            // 
+            // labAverage
+            // 
+            this.labAverage.AutoSize = true;
+            this.labAverage.Location = new System.Drawing.Point(130, 441);
+            this.labAverage.Name = "labAverage";
+            this.labAverage.Size = new System.Drawing.Size(28, 13);
+            this.labAverage.TabIndex = 65;
+            this.labAverage.Text = "50.9";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(74, 441);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(53, 13);
+            this.label18.TabIndex = 64;
+            this.label18.Text = "Average :";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(90, 409);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(37, 13);
+            this.label17.TabIndex = 63;
+            this.label17.Text = "Total :";
+            // 
+            // btnComment
+            // 
+            this.btnComment.Location = new System.Drawing.Point(491, 322);
+            this.btnComment.Name = "btnComment";
+            this.btnComment.Size = new System.Drawing.Size(75, 23);
+            this.btnComment.TabIndex = 62;
+            this.btnComment.Text = "Comment";
+            this.btnComment.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(491, 399);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 61;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Location = new System.Drawing.Point(491, 279);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(75, 23);
+            this.btnAverage.TabIndex = 60;
+            this.btnAverage.Text = "Get Average";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.btnAverage_Click);
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.Location = new System.Drawing.Point(491, 236);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(75, 23);
+            this.btnTotal.TabIndex = 59;
+            this.btnTotal.Text = "Get Total";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            this.btnTotal.Click += new System.EventHandler(this.btnTotal_Click);
+            // 
+            // btnGrade
+            // 
+            this.btnGrade.Location = new System.Drawing.Point(491, 193);
+            this.btnGrade.Name = "btnGrade";
+            this.btnGrade.Size = new System.Drawing.Size(75, 23);
+            this.btnGrade.TabIndex = 58;
+            this.btnGrade.Text = "Get Grade";
+            this.btnGrade.UseVisualStyleBackColor = true;
+            this.btnGrade.Click += new System.EventHandler(this.btnGrade_Click);
+            // 
+            // labReligion
+            // 
+            this.labReligion.AutoSize = true;
+            this.labReligion.Location = new System.Drawing.Point(355, 360);
+            this.labReligion.Name = "labReligion";
+            this.labReligion.Size = new System.Drawing.Size(14, 13);
+            this.labReligion.TabIndex = 57;
+            this.labReligion.Text = "A";
+            // 
+            // labHistory
+            // 
+            this.labHistory.AutoSize = true;
+            this.labHistory.Location = new System.Drawing.Point(355, 318);
+            this.labHistory.Name = "labHistory";
+            this.labHistory.Size = new System.Drawing.Size(14, 13);
+            this.labHistory.TabIndex = 56;
+            this.labHistory.Text = "A";
+            // 
+            // labScience
+            // 
+            this.labScience.AutoSize = true;
+            this.labScience.Location = new System.Drawing.Point(355, 279);
+            this.labScience.Name = "labScience";
+            this.labScience.Size = new System.Drawing.Size(14, 13);
+            this.labScience.TabIndex = 55;
+            this.labScience.Text = "A";
+            // 
+            // labMatha
+            // 
+            this.labMatha.AutoSize = true;
+            this.labMatha.Location = new System.Drawing.Point(355, 242);
+            this.labMatha.Name = "labMatha";
+            this.labMatha.Size = new System.Drawing.Size(14, 13);
+            this.labMatha.TabIndex = 54;
+            this.labMatha.Text = "A";
+            // 
+            // labEnglish
+            // 
+            this.labEnglish.AutoSize = true;
+            this.labEnglish.Location = new System.Drawing.Point(355, 206);
+            this.labEnglish.Name = "labEnglish";
+            this.labEnglish.Size = new System.Drawing.Size(14, 13);
+            this.labEnglish.TabIndex = 53;
+            this.labEnglish.Text = "A";
+            // 
+            // labSinhala
+            // 
+            this.labSinhala.AutoSize = true;
+            this.labSinhala.Location = new System.Drawing.Point(355, 172);
+            this.labSinhala.Name = "labSinhala";
+            this.labSinhala.Size = new System.Drawing.Size(14, 13);
+            this.labSinhala.TabIndex = 52;
+            this.labSinhala.Text = "A";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(344, 137);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(36, 13);
+            this.label10.TabIndex = 51;
+            this.label10.Text = "Grade";
+            // 
+            // txtReligion
+            // 
+            this.txtReligion.Location = new System.Drawing.Point(133, 357);
+            this.txtReligion.Name = "txtReligion";
+            this.txtReligion.Size = new System.Drawing.Size(155, 20);
+            this.txtReligion.TabIndex = 50;
+            // 
+            // txtHistory
+            // 
+            this.txtHistory.Location = new System.Drawing.Point(133, 315);
+            this.txtHistory.Name = "txtHistory";
+            this.txtHistory.Size = new System.Drawing.Size(155, 20);
+            this.txtHistory.TabIndex = 49;
+            // 
+            // txtScience
+            // 
+            this.txtScience.Location = new System.Drawing.Point(133, 276);
+            this.txtScience.Name = "txtScience";
+            this.txtScience.Size = new System.Drawing.Size(155, 20);
+            this.txtScience.TabIndex = 48;
+            // 
+            // txtMatha
+            // 
+            this.txtMatha.Location = new System.Drawing.Point(133, 239);
+            this.txtMatha.Name = "txtMatha";
+            this.txtMatha.Size = new System.Drawing.Size(155, 20);
+            this.txtMatha.TabIndex = 47;
+            // 
+            // txtEnglish
+            // 
+            this.txtEnglish.Location = new System.Drawing.Point(133, 203);
+            this.txtEnglish.Name = "txtEnglish";
+            this.txtEnglish.Size = new System.Drawing.Size(155, 20);
+            this.txtEnglish.TabIndex = 46;
+            // 
+            // txtSinhala
+            // 
+            this.txtSinhala.Location = new System.Drawing.Point(133, 169);
+            this.txtSinhala.Name = "txtSinhala";
+            this.txtSinhala.Size = new System.Drawing.Size(155, 20);
+            this.txtSinhala.TabIndex = 45;
+            // 
+            // txtStuNo
+            // 
+            this.txtStuNo.Location = new System.Drawing.Point(133, 89);
+            this.txtStuNo.Name = "txtStuNo";
+            this.txtStuNo.Size = new System.Drawing.Size(457, 20);
+            this.txtStuNo.TabIndex = 44;
+            // 
+            // txtStuName
+            // 
+            this.txtStuName.Location = new System.Drawing.Point(133, 58);
+            this.txtStuName.Name = "txtStuName";
+            this.txtStuName.Size = new System.Drawing.Size(457, 20);
+            this.txtStuName.TabIndex = 43;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(180, 137);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(48, 13);
+            this.label9.TabIndex = 42;
+            this.label9.Text = "Subjects";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(76, 360);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 13);
+            this.label8.TabIndex = 41;
+            this.label8.Text = "Religion :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(75, 279);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 13);
+            this.label7.TabIndex = 40;
+            this.label7.Text = "Science :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(54, 242);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 13);
+            this.label6.TabIndex = 39;
+            this.label6.Text = "Mathematics :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(80, 210);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 38;
+            this.label5.Text = "English :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(82, 322);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 37;
+            this.label4.Text = "History :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "Sinhala :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "Student No :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Student Name :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 484);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.labTotal);
+            this.Controls.Add(this.labAverage);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.btnComment);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.btnGrade);
+            this.Controls.Add(this.labReligion);
+            this.Controls.Add(this.labHistory);
+            this.Controls.Add(this.labScience);
+            this.Controls.Add(this.labMatha);
+            this.Controls.Add(this.labEnglish);
+            this.Controls.Add(this.labSinhala);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtReligion);
+            this.Controls.Add(this.txtHistory);
+            this.Controls.Add(this.txtScience);
+            this.Controls.Add(this.txtMatha);
+            this.Controls.Add(this.txtEnglish);
+            this.Controls.Add(this.txtSinhala);
+            this.Controls.Add(this.txtStuNo);
+            this.Controls.Add(this.txtStuName);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label labTotal;
+        private System.Windows.Forms.Label labAverage;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Button btnComment;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.Button btnGrade;
+        private System.Windows.Forms.Label labReligion;
+        private System.Windows.Forms.Label labHistory;
+        private System.Windows.Forms.Label labScience;
+        private System.Windows.Forms.Label labMatha;
+        private System.Windows.Forms.Label labEnglish;
+        private System.Windows.Forms.Label labSinhala;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtReligion;
+        private System.Windows.Forms.TextBox txtHistory;
+        private System.Windows.Forms.TextBox txtScience;
+        private System.Windows.Forms.TextBox txtMatha;
+        private System.Windows.Forms.TextBox txtEnglish;
+        private System.Windows.Forms.TextBox txtSinhala;
+        private System.Windows.Forms.TextBox txtStuNo;
+        private System.Windows.Forms.TextBox txtStuName;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ StudentResult1/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentResult1
+{
+
+    public partial class Form1 : Form
+    {
+
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void DefineVariable() {
+           int Sinhala = Convert.ToInt32(txtSinhala.Text);
+           int English = Convert.ToInt32(txtEnglish.Text);
+           int Matha = Convert.ToInt32(txtMatha.Text);
+           int Science = Convert.ToInt32(txtScience.Text);
+           int History = Convert.ToInt32(txtHistory.Text);
+           int Religion = Convert.ToInt32(txtReligion.Text);
+
+            int Total = Sinhala + English + Matha + Science + History + Religion;
+
+            float Average = Total / 6;
+            labAverage.Text = Average.ToString();
+    
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtStuName.Text = String.Empty;
+            txtStuNo.Text = String.Empty;
+
+            txtSinhala.Text = String.Empty;
+            txtEnglish.Text = String.Empty;
+            txtMatha.Text = String.Empty;
+            txtScience.Text = String.Empty;
+            txtHistory.Text = String.Empty;
+            txtReligion.Text = String.Empty;
+
+            labTotal.Text = String.Empty;
+            labAverage.Text = String.Empty;
+
+            labSinhala.Text = String.Empty;
+            labEnglish.Text = String.Empty;
+            labMatha.Text = String.Empty;
+            labScience.Text = String.Empty;
+            labHistory.Text = String.Empty;
+            labReligion.Text = String.Empty;
+
+            txtReligion.Clear();
+
+        }
+
+        private void btnGrade_Click(object sender, EventArgs e)
+        {
+            int Sinhala = Convert.ToInt32(txtSinhala.Text);
+            int English = Convert.ToInt32(txtEnglish.Text);
+            int Matha = Convert.ToInt32(txtMatha.Text);
+            int Science = Convert.ToInt32(txtScience.Text);
+            int History = Convert.ToInt32(txtHistory.Text);
+            int Religion = Convert.ToInt32(txtReligion.Text);
+
+            if (Sinhala < 35)
+            {
+                labSinhala.Text = "F";
+            }
+            else if (Sinhala < 55) {
+                labSinhala.Text = "S";
+            }
+            else if (Sinhala < 65)
+            {
+                labSinhala.Text = "C";
+            }
+            else if (Sinhala < 75)
+            {
+                labSinhala.Text = "B";
+            }
+            else
+            {
+                labSinhala.Text = "A";
+            }
+
+            
+            if (English < 35)
+            {
+                labEnglish.Text = "F";
+            }
+            else if (English < 55)
+            {
+                labEnglish.Text = "S";
+            }
+            else if (English < 65)
+            {
+                labEnglish.Text = "C";
+            }
+            else if (English < 75)
+            {
+                labEnglish.Text = "B";
+            }
+            else
+            {
+                labEnglish.Text = "A";
+            }
+
+            
+            if (Matha < 35)
+            {
+                labMatha.Text = "F";
+            }
+            else if (Matha < 55)
+            {
+                labMatha.Text = "S";
+            }
+            else if (Matha < 65)
+            {
+                labMatha.Text = "C";
+            }
+            else if (Matha < 75)
+            {
+                labMatha.Text = "B";
+            }
+            else
+            {
+                labMatha.Text = "A";
+            }
+
+            
+            if (Science < 35)
+            {
+                labScience.Text = "F";
+            }
+            else if (Science < 55)
+            {
+                labScience.Text = "S";
+            }
+            else if (Science < 65)
+            {
+                labScience.Text = "C";
+            }
+            else if (Science < 75)
+            {
+                labScience.Text = "B";
+            }
+            else
+            {
+                labScience.Text = "A";
+            }
+
+            
+            if (History < 35)
+            {
+                labHistory.Text = "F";
+            }
+            else if (History < 55)
+            {
+                labHistory.Text = "S";
+            }
+            else if (History < 65)
+            {
+                labHistory.Text = "C";
+            }
+            else if (History < 75)
+            {
+                labHistory.Text = "B";
+            }
+            else
+            {
+                labHistory.Text = "A";
+            }
+
+            
+            if (Religion < 35)
+            {
+                labReligion.Text = "F";
+            }
+            else if (Religion < 55)
+            {
+                labReligion.Text = "S";
+            }
+            else if (Religion < 65)
+            {
+                labReligion.Text = "C";
+            }
+            else if (Religion < 75)
+            {
+                labReligion.Text = "B";
+            }
+            else
+            {
+                labReligion.Text = "A";
+            }
+
+
+
+
+        }
+
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+            int Sinhala = Convert.ToInt32(txtSinhala.Text);
+            int English = Convert.ToInt32(txtEnglish.Text);
+            int Matha = Convert.ToInt32(txtMatha.Text);
+            int Science = Convert.ToInt32(txtScience.Text);
+            int History = Convert.ToInt32(txtHistory.Text);
+            int Religion = Convert.ToInt32(txtReligion.Text);
+
+            int Total = Sinhala + English + Matha + Science + History + Religion;
+
+            labTotal.Text = Total.ToString();
+
+        }
+
+        public void btnAverage_Click(object sender, EventArgs e)
+        {
+            DefineVariable();
+
+        }
+    }
+}
